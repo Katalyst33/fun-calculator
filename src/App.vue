7d1468b233297bb0f3c70a77f69a7306be2ef5e2
tanya <tanyareedy3@gmail.com>
2020-05-06 07:16:57 +0400

    randomise number and get answer 😻
    >> randomise operator

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-  body{
-    background-color: #b794fc;
-  }
+
 </style>
--- src/components/calculateFun.vue
+++ src/components/calculateFun.vue
@@ -5,6 +5,8 @@
                 Calculator Game
             </h1>
 
+            <p id="answer" class="has-background-grey-lighter py-1">Answer:{{answer}}</p>
+
 
             <section class="container mx-5 px-5 py-5">
                 <div class="area">
@@ -12,7 +14,7 @@
                         <h1 class="is-size-1">{{valueA}}</h1>
                     </div>
                     <div class="box-2">
-                        <h1 id="operator"  class="is-size-1 has-text-danger">{{operation}}</h1>
+                        <h1 id="operator" class="has-text-weight-bold has-text-danger">{{operator}}</h1>
 
 
                     </div>
@@ -25,7 +27,9 @@
 
                     </div>
                     <div class="box-5">
-                        <h1 class="is-size-1">{{answer}}</h1>
+
+                        <input v-model="answer" class="input is-medium" type="number">
+                        {{operator}}
 
                     </div>
                 </div>
@@ -33,16 +37,22 @@
                 <div class="py-5">
 
                     <div>
+                        <button @click="refresh" class="button is-success">Refresh</button>
+
                         <div class="select mx-5">
-                            <select v-model="operation">
-                                <option value="select operator">Select operator</option>
+                            <select
+                                    @change="selectOperator"
+                                    v-model="operator">
+                                <option disabled value="">Please select one
+                                </option>
                                 <option value="+">+ (Addition)</option>
                                 <option value="-">-(Subtraction)</option>
                                 <option value="/">/(Division)</option>
                                 <option value="*">*(Multiply)</option>
                             </select>
                         </div>
-                        <button @click="calculate" class="button is-warning">Check</button>
+                        <button @click="equals" class="button is-dark mx-4">Get answer</button>
+                        <button @click="equals" class="button is-warning">submit</button>
                     </div>
                     <h1 class="has-text-success is-size-1">Score:50</h1>
                 </div>
@@ -58,36 +68,115 @@
     export default {
         data() {
             return {
-                valueA:3,
-                valueB:4,
-                answer:null,
-                operation:null
+                valueA: 3,
+                valueB: 4,
+                min: 1,
+                max: 10,
+                answer: null,
+                operator: "???",
+                operators: ['-', '+', '*', '/'],
+                operation: null,
             }
         },
 
+        created: function () {
+            this.getRandomNumber()
+            // this.randomiseOperator()
+        },
 
-        methods:{
-            calculate(){
-                this.answer = ` ${this.valueA } ${this.operation} ${this.valueB}`
+        methods: {
 
-                console.log(this.answer)
+            refresh() {
+                this.getRandomNumber()
+                // this.randomiseOperator()
+            },
+
+            selectOperator() {
+                if (this.operator === '*') {
+                    this.times()
+                }
+                if (this.operator === '/') {
+                    this.divide()
+                }
+                if (this.operator === '+') {
+                    this.add()
+                }
+                if (this.operator === '-') {
+                    this.minus()
+                }
+
+            },
+
+
+            times() {
+                this.operation = (a, b) => a * b;
+
+            },
+            minus() {
+                this.operation = (a, b) => a - b;
+
+            },
+            add() {
+                this.operation = (a, b) => a + b;
+
+
+            },
+            divide() {
+                this.operation = (a, b) => a / b;
+
+
+            },
+
+            equals() {
+                this.answer = `${this.operation(
+                    parseFloat(this.valueA),
+                    parseFloat(this.valueB),
+                )}`;
+
+            },
+            getRandomNumber() {
+                this.valueA = this.generateNumber()
+                this.valueB = this.generateNumber()
+                // this.operator = this.randomiseOperator()
+            },
+
+            generateNumber() {
+                return Math.floor(Math.random() * (10) + 1);
+
+                /*  },
+            randomiseOperator() {
+                let selector = Math.floor(Math.random() * this.operators.length);
+                this.operator = this.operators[selector]
+
+            }
+*/
+
             }
 
         }
-
     }
 </script>
 
 <style scoped>
 
-    .area{
+    .area {
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
 
-    #operator{
-        font-size: 200px;
+    .input {
+        width: 35%;
+    }
+
+    #operator {
+        font-size: 60px;
+    }
+
+    #answer {
+        width: 30%;
+        margin: 0 auto;
+
     }
 
 
